92844f26f83bb5d07357ab1fac02b056cce5592e
Kevin-Pérez-Baena- <keperba@gmail.com>
2020-12-11 17:19:15 +0100

    Script del enemic Melee

---
+++ HonorDuty/Assets/Scripts/EnemyMelee.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMelee
+   : MonoBehaviour
+{
+    private Transform target;
+    private float speed;
+
+    //In the method start the enemy detect PlayerController.
+    void Start()
+    {
+        target = FindObjectOfType<PlayerController>().transform;
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    //This method the enemy follow the player.
+    public void FollowPlayer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+}
