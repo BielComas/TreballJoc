1518894656680d26349bbceec2bc91fa519fd8eb
bielcomas <bielcomas1@gmail.com>
2020-12-17 13:27:53 +0100

    moviment player

--- HonorDuty/Assets/Scripts/PlayerController.cs
+++ HonorDuty/Assets/Scripts/PlayerController.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] Transform player;
     Vector2 direction;
+    public float velocity = 4f;
+    Rigidbody2D rb;
     [SerializeField] int lifesPlayer = 100;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = player.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        direction.x = Input.GetAxis("Horitzontal");
+        direction.x = Input.GetAxis("Horizontal");
         direction.y = Input.GetAxis("Vertical");
 
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + direction * velocity * Time.deltaTime);
     }
 }
