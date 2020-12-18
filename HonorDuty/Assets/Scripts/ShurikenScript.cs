cff2139070b126b01f89f83f41677c048a56383a
Robert-Payan <robertpayan99@gmail.com>
2020-12-18 10:50:50 +0100

    Añadido el codigo en el script del shuriken y su sprite

--- HonorDuty/Assets/Scripts/ShurikenScript.cs
+++ HonorDuty/Assets/Scripts/ShurikenScript.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShurikenScript : MonoBehaviour
 {
+    public float speed;
+    public Rigidbody2D myRigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Setup(Vector2 velocity, Vector3 direction)
     {
-        
+        myRigidBody.velocity = velocity.normalized * speed;
+        transform.rotation = Quaternion.Euler(direction);
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    internal void Setup(Vector2 temp, Func<Vector3> chooseShurikenDirection)
+    {
+        throw new NotImplementedException();
     }
 }
