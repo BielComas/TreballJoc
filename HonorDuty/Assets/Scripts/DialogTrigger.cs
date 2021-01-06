34298d6328d72d26a2e12ae16d49e12d3bf5e941
bielcomas <bielcomas1@gmail.com>
2021-01-06 19:53:16 +0100

    Dialegs introducció versió 1 implementats

---
+++ HonorDuty/Assets/Scripts/DialogTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTrigger : MonoBehaviour
+{
+    [SerializeField] Canvas canvas;
+    [SerializeField] Dialog dialogue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.GetComponent<Canvas>().enabled = false;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            canvas.GetComponent<Canvas>().enabled = true;
+            FindObjectOfType<DialogManager>().StartDialogue(dialogue);
+        }
+    }
+}
