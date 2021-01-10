12121522a686a7058b7bea05650fa55c2bc19f23
Kevin-Pérez-Baena- <keperba@gmail.com>
2021-01-10 16:33:25 +0100

    Animacions del player

--- HonorDuty/Assets/Scripts/SwordScript.cs
+++ HonorDuty/Assets/Scripts/SwordScript.cs
@@ -2,27 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sword : MonoBehaviour
+public class SwordScript : MonoBehaviour
 {
     public float damage = 1f;
     public Rigidbody2D rb2d;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     //If the sword collision with enemy, enemy die
     public void OnTriggerEnter2D(Collider2D other)
     {
-
+       if (Input.GetKeyDown("Button0"))
         if (other.gameObject.CompareTag("Enemy"))
         {
             //DeathAnimation
