db849f24305313ba7e51a5c80e39ada7e5a2d266
Kevin-Pérez-Baena- <keperba@gmail.com>
2021-04-18 15:54:06 +0200

    solucionat

---
+++ HonorDuty/Assets/Scripts/Dialog.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialog : MonoBehaviour
+{
+    [SerializeField] public string name;
+
+    [TextArea(3, 10)]
+    [SerializeField] public string[] sentenceList;
+}
+
---
+++ HonorDuty/Assets/Scripts/DialogManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+    public GameObject canvas;
+
+
+    [SerializeField] Queue<string> sentences;
+
+
+    private void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialog dialogue)
+    {
+        
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentenceList)
+        {
+            sentences.Enqueue(sentence);
+        }
+    }
+    public void ShowNextSentence()
+    {
+        if (sentences.Count <= 0)
+        {
+            canvas.SetActive(false);
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+}
