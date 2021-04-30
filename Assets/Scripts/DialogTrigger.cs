23f38ad474125381799ac8b0bedca8a5389c4b98
Kevin-Pérez-Baena- <keperba@gmail.com>
2021-04-30 11:58:22 +0200

    En procés d'arreglar del tot les animacions deshabilitades dins dels dialegs

--- Assets/Scripts/DialogTrigger.cs
+++ Assets/Scripts/DialogTrigger.cs
@@ -7,11 +7,14 @@ public class DialogTrigger : MonoBehaviour
     [SerializeField] Canvas canvas;
     [SerializeField] Dialog dialogue;
     public GameObject DialogueStart;
+    private Animator anim;
+    private PlayerController playerController;
 
     // Start is called before the first frame update
    public void Start()
     {
         canvas.GetComponent<Canvas>().enabled = false;
+        playerController.GetComponent<PlayerController>();
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
@@ -19,13 +22,18 @@ public class DialogTrigger : MonoBehaviour
         if (collision.transform.tag == "Player")
         {
             canvas.GetComponent<Canvas>().enabled = true;
-            FindObjectOfType<DialogManager>().StartDialogue(dialogue);
             Time.timeScale = 0f;
+            FindObjectOfType<DialogManager>().StartDialogue(dialogue);
             GameObject.Destroy(DialogueStart);
 
             if (DialogueStart == null)
             {
               Time.timeScale = 1f;
+            }
+
+            if(DialogueStart == true)
+            {
+                playerController.enabled = false;
             }
         }
     }
@@ -33,6 +41,7 @@ public class DialogTrigger : MonoBehaviour
     public void OnTriggerExit2D(Collider2D collision)
     {
         canvas.GetComponent<Canvas>().enabled = false;
+        playerController.enabled = true;
         Time.timeScale = 1f;
     }
 }
